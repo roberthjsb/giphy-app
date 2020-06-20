abd3defc3ca15b77ddd57131aec34092e765d8cd
Roberth Salazar <roberth.salazar@gmail.com>
2020-06-19 20:05:22 -0400

    productivo

---
+++ docs/precache-manifest.69c67dd5759d8d3b7b67d1b3c2a93d6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc5716dba606edfcafc1f3a9073c692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ab482695469f8a90b0b",
+    "url": "/static/css/main.d18e4597.chunk.css"
+  },
+  {
+    "revision": "63b85b6720d81c384743",
+    "url": "/static/js/2.4e373f12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e373f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab482695469f8a90b0b",
+    "url": "/static/js/main.0aed3b3b.chunk.js"
+  },
+  {
+    "revision": "e13af855f2385358fc05",
+    "url": "/static/js/runtime-main.17c06fbb.js"
+  }
+]);
